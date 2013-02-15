9e8aba673b728d0af2b210411f950dc9ff8e306b
mpollack <mpollack@vmware.com>
2013-02-16 04:40:54 +0800

    put all property files in single property-placeholder.  add default 'true' filter to noop processor

--- server/src/main/java/org/springframework/data/hadoop/server/SpringDataServer.java
+++ server/src/main/java/org/springframework/data/hadoop/server/SpringDataServer.java
@@ -31,13 +31,15 @@ public class SpringDataServer {
 			parser.printUsage(System.err);
 			System.exit(-1);
 		}
+		
+		
 		//TODO merge into two dispatcher servlets in web.xml
 		ExecutorService executorService = Executors.newFixedThreadPool(4, new CustomizableThreadFactory("server-"));
 		List<Callable<Void>> tasks = new ArrayList<Callable<Void>>();
 		tasks.add(createIntegrationCallable(options));
 		//tasks.add(createAdminCallable());
 		List<Future<Void>> f = executorService.invokeAll(tasks);
-
+		
 	}
 
 	/**
