869e34981ba21c2993493c8f66df301a2a69694b
Thomas Risberg <trisberg@vmware.com>
2013-02-15 12:17:25 -0500

    Adding server to shell and root pom

--- server/src/main/java/org/springframework/data/hadoop/server/SpringDataServer.java
+++ server/src/main/java/org/springframework/data/hadoop/server/SpringDataServer.java
@@ -6,8 +6,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
 
 import org.springframework.data.hadoop.jetty.BatchAdminServer;
 import org.springframework.data.hadoop.jetty.IntegrationServer;
@@ -25,7 +23,7 @@ public class SpringDataServer {
 		tasks.add(createIntegrationCallable());
 		tasks.add(createAdminCallable());
 		List<Future<Void>> f = executorService.invokeAll(tasks);
-		
+
 	}
 
 	/**
