18c7f60259c6a4264113bb2c79bcda78fbcd89ad
mpollack <mpollack@vmware.com>
2013-02-16 03:06:56 +0800

    Add command line option support

--- server/src/main/java/org/springframework/data/hadoop/server/SpringDataServer.java
+++ server/src/main/java/org/springframework/data/hadoop/server/SpringDataServer.java
@@ -7,22 +7,34 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.hadoop.jetty.BatchAdminServer;
-import org.springframework.data.hadoop.jetty.IntegrationServer;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
-
 
 public class SpringDataServer {
 
+	private static final Log log = LogFactory.getLog(SpringDataServer.class);
+
 	public static void main(String[] args) throws Exception {
 		
-		
+		SpringDataServerOptions options = new  SpringDataServerOptions();
+		CmdLineParser parser = new CmdLineParser(options);
+		try {
+			parser.parseArgument(args);
+		} catch (CmdLineException e) {
+			log.error(e.getMessage());
+			parser.printUsage(System.err);
+			System.exit(-1);
+		}
 		//TODO merge into two dispatcher servlets in web.xml
 		ExecutorService executorService = Executors.newFixedThreadPool(4, new CustomizableThreadFactory("server-"));
 		List<Callable<Void>> tasks = new ArrayList<Callable<Void>>();
-		tasks.add(createIntegrationCallable());
+		tasks.add(createIntegrationCallable(options));
 		tasks.add(createAdminCallable());
 		List<Future<Void>> f = executorService.invokeAll(tasks);
 
@@ -31,13 +43,13 @@ public class SpringDataServer {
 	/**
 	 * 
 	 */
-	private static Callable<Void> createIntegrationCallable() {
+	private static Callable<Void> createIntegrationCallable(final SpringDataServerOptions options) {
 		Callable<Void> siCallable = new Callable<Void>() {
 
 			@Override
 			public Void call() throws Exception {
 				AbstractApplicationContext context = new ClassPathXmlApplicationContext(
-						"/syslog-hdfs/application-context.xml");
+						"/" + options.getAppConfig() + "/application-context.xml");
 				context.registerShutdownHook();	
 				return null;
 			}
---
+++ server/src/main/java/org/springframework/data/hadoop/server/SpringDataServerOptions.java
@@ -0,0 +1,18 @@
+package org.springframework.data.hadoop.server;
+
+import org.kohsuke.args4j.Option;
+
+public class SpringDataServerOptions {
+
+	@Option(name="-appConfig", usage="the application configuration directory")
+	private String appConfig = "syslog-hdfs";
+
+	/**
+	 * @return the appConfig
+	 */
+	public String getAppConfig() {
+		return appConfig;
+	}
+	
+	
+}
